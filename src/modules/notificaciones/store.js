7d1fe17253fab8d081f48b049de9a03c01227014
LUZWIN <luzwinzc@gmail.com>
2025-07-09 03:33:55 -0500

    Fix: diseño y store de notificaciones funcionando

---
+++ src/modules/notificaciones/store.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+
+export const useNotificacionesStore = defineStore('notificaciones', {
+  state: () => ({
+    lista: [],
+  }),
+  actions: {
+    // Puedes agregar acciones aquí si lo necesitas
+  },
+})
