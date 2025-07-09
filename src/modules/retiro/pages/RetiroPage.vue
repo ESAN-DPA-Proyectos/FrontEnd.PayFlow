87cf1a55d3595cef2e4587edeecbc1bb886ad3b0
willtl2000 <wilber.ticona@outlook.com>
2025-07-08 19:50:50 -0500

    Solicitud de retiro en fase simulación

---
+++ src/modules/retiro/pages/RetiroPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="q-pa-xl">
+    <div class="text-h5">Registrar Solicitud de Retiro</div>
+    <q-form class="q-mt-md">
+      <q-select
+        label="Tipo de Fondo"
+        :options="fondos"
+        v-model="fondoSeleccionado"
+        filled
+        class="q-mb-md"
+      />
+
+      <q-input label="Monto a retirar (S/)" type="number" filled class="q-mb-md" />
+
+      <q-input label="Cuenta destino" filled class="q-mb-md" />
+
+      <q-input label="Nombre del beneficiario" filled class="q-mb-md" />
+
+      <q-input label="Concepto" type="textarea" filled class="q-mb-md" />
+
+      <q-btn label="Solicitar Retiro" color="primary" />
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'RetiroPage',
+  data() {
+    return {
+      fondoSeleccionado: null,
+      fondos: ['Fondo 1', 'Fondo 2', 'Fondo 3'],
+    }
+  },
+}
+</script>
